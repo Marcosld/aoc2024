f5fcfc1b6ef0cce4c3f8b0e75c6b204862d8e877
Marcos Luis Delgado <mluis@tuenti.com>
2024-12-18 12:08:37 +0100

    main: Fixed P16

--- 16.mjs
+++ 16.mjs
@@ -1,58 +1,74 @@
-import { readInput, getStraightAdjacentPositions } from "./utils.mjs";
+import {
+  readInput,
+  getStraightAdjacentPositions,
+  DefaultObj,
+} from "./utils.mjs";
 
 const input = readInput(import.meta);
 const map = input.split("\n").map((l) => l.split(""));
-const vertexes = [];
-const graph = {}
+const S = [[map.length - 2, 1], ">"];
+const E = [1, map[1].length - 2];
 
 const DIRS = ">v<^";
 
-for (const [i, line] of map.entries()) {
-  for (const [j, c] of line.entries()) {
-    if (c === ".") {
-      const ways = getStraightAdjacentPositions([i, j])
-        .map((pos, i) => [pos, DIRS[i]])
-        .filter(([[i, j]]) => map[i][j] === ".")
-        .map(([, dir]) => dir)
-        .join("");
-      if (ways !== "v^" && ways !== "><") {
-        ways
-          .split("")
-          .forEach((dir) => vertexes.push(JSON.stringify([i, j, dir])));
+const str = (val) => JSON.stringify(val);
+const prs = (val) => JSON.parse(val);
+
+const getPossibleMoves = (pos, dir, score) =>
+  getStraightAdjacentPositions(pos)
+    .map((newPos, i) => [newPos, i])
+    .filter(([[i, j]]) => map[i][j] !== "#")
+    .map(([newPos, newDirI]) => {
+      const newDir = DIRS[newDirI];
+      if (dir === newDir) {
+        return [newPos, newDir, score + 1];
       }
-    }
+      const dirsDistance = Math.abs(DIRS.indexOf(dir) - DIRS.indexOf(newDir));
+      const nOfTurns = dirsDistance > 2 ? 1 : dirsDistance;
+      return [pos, newDir, score + 1000 * nOfTurns];
+    });
+
+const solve = () => {
+  const positions = new Set([str(S)]);
+  const seen = DefaultObj(Infinity);
+  const previouses = DefaultObj(new Set());
+  let bestEnds = [];
+  seen[str(S)] = 0;
+  while (positions.size) {
+    const posDirStr = positions.keys().next().value;
+    positions.delete(posDirStr);
+    const [pos, dir] = prs(posDirStr);
+    const score = seen[posDirStr];
+    const possibleMoves = getPossibleMoves(pos, dir, score);
+    possibleMoves.forEach(([nextPos, nextDir, nextScore]) => {
+      const nextPosDirStr = str([nextPos, nextDir]);
+      if (nextScore < seen[nextPosDirStr]) {
+        seen[nextPosDirStr] = nextScore;
+        previouses[nextPosDirStr] = new Set();
+        if (pos[0] === E[0] && pos[1] === E[1]) {
+          bestEnds = [];
+        }
+      }
+      if (nextScore <= seen[nextPosDirStr]) {
+        previouses[nextPosDirStr].add(posDirStr);
+        positions.add(nextPosDirStr);
+        if (pos[0] === E[0] && pos[1] === E[1]) {
+          bestEnds.push(posDirStr);
+        }
+      }
+    });
   }
-}
+  return [seen[bestEnds[0]], traverse(bestEnds, previouses)];
+};
 
-const expand = ([i, j], [di, dj]) => {
+const traverse = (keys, previouses, visited = new Set()) => {
+  for (const key of keys) {
+    visited.add(str(prs(key)[0]));
+    traverse(previouses[key], previouses, visited);
+  }
+  return visited.size;
+};
 
-}
-
-for (const v of vertexes) {
-  graph[v] =
-}
-
-
-
-// const solve1 = () => {
-//   let positions = [[S, ">", 0]];
-//   let lowest = Infinity;
-//   const seen = {}; // just lowest for each position
-//   while (positions.length) {
-//     positions = positions.sort((a, b) => estimate(...b) - estimate(...a));
-//     const [pos, dir, score] = positions.shift();
-//     if (pos[0] === E[0] && pos[1] === E[1]) {
-//       lowest = Math.min(lowest, score);
-//     }
-//     const keySeen = JSON.stringify(pos);
-//     if (keySeen in seen && seen[keySeen] < score) {
-//       continue;
-//     }
-//     seen[keySeen] = score;
-//     const possibleMoves = getPossibleMoves(pos, dir, score);
-//     positions.push(...possibleMoves);
-//   }
-//   return lowest;
-// };
-//
-// console.log(solve1());
+console.time("solve");
+console.log(solve().join("\n"));
+console.timeEnd("solve");
