c562391554cfd11fd3f2fb172512d19ff1631db3
Marcos Luis Delgado <mluis@tuenti.com>
2024-12-23 01:23:00 +0100

    main: P22

--- 22.mjs
+++ 22.mjs
@@ -3,11 +3,14 @@ import {
   getStraightAdjacentPositions,
   str,
   cache,
+  DefaultObj,
 } from "./utils.mjs";
 
 const input = readInput(import.meta);
 
 const pruner = 16777216n;
+
+const bananaCounts = DefaultObj(0n);
 
 const getNextSecret = (n) => {
   const a = (n ^ (n * 64n)) % pruner;
@@ -18,13 +21,39 @@ const getNextSecret = (n) => {
 const solve = () => {
   let acc = 0n;
   for (const num of input.split("\n").map(BigInt)) {
+    const seenKeys = new Set();
+    let varianceKey = "";
     let secret = num;
     for (let i = 0; i < 2000; i++) {
-      secret = getNextSecret(secret);
+      const temp = getNextSecret(secret);
+      const bananas = temp % 10n;
+      const variance = bananas - (secret % 10n);
+      varianceKey += variance >= 0 ? "+" + variance : variance;
+      if (varianceKey.length > 8) {
+        varianceKey = varianceKey.slice(2);
+      }
+      if (varianceKey.length === 8 && !seenKeys.has(varianceKey)) {
+        bananaCounts[varianceKey] += bananas;
+        seenKeys.add(varianceKey);
+      }
+      secret = temp;
     }
     acc += secret;
   }
-  return acc;
+  return [
+    acc,
+    Object.values(bananaCounts).sort((a, b) => {
+      if (a < b) {
+        return 1;
+      }
+      if (a > b) {
+        return -1;
+      }
+      return 0;
+    })[0],
+  ];
 };
 
-console.log(solve());
+console.time("a");
+console.log(solve().join("\n"));
+console.timeEnd("a");
