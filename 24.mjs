b0ec83c825b82ddf85b7cbe52ed04bc77480196e
Marcos Luis Delgado <mluis@tuenti.com>
2024-12-24 21:53:26 +0100

    main: Finished p24

--- 24.mjs
+++ 24.mjs
@@ -1,10 +1,4 @@
-import {
-  readInput,
-  getStraightAdjacentPositions,
-  str,
-  cache,
-  DefaultObj,
-} from "./utils.mjs";
+import { readInput } from "./utils.mjs";
 
 const input = readInput(import.meta);
 const [resultsStr, doorsStr] = input.split("\n\n");
@@ -24,7 +18,8 @@ for (const resultStr of resultsStr.split("\n")) {
 
 for (const doorStr of doorsStr.split("\n")) {
   const [kStr, res] = doorStr.split(" -> ");
-  doors.push([...kStr.split(" "), res]);
+  const [op1, op, op2] = kStr.split(" ");
+  doors.push([op1, op, op2, res]);
 }
 
 const solve1 = () => {
@@ -55,26 +50,51 @@ const solve1 = () => {
   );
 };
 
-const getBinary = (results, keyStartsWith) =>
-  Object.keys(results)
-    .filter((k) => k.startsWith(keyStartsWith))
-    .sort()
-    .toReversed()
-    .map((k) => results[k]);
+const pad = (n) => n.toString().padStart(2, "0");
 
-const parseBinary = (binary) => parseInt(binary.join(""), 2);
-
-const solve2 = () => {
-  const xBinary = getBinary(results, "x");
-  const yBinary = getBinary(results, "y");
-  const expectedZBinary = (parseBinary(xBinary) + parseBinary(yBinary))
-    .toString(2)
-    .split("");
-  console.log(xBinary, parseBinary(xBinary));
-  console.log(yBinary, parseBinary(yBinary));
-  console.log(parseBinary(xBinary) + parseBinary(yBinary));
-  console.log(expectedZBinary);
+const findDoor = (op1, op, op2) => {
+  const foundDoor = doors.find(
+    ([dop1, dop, dop2]) =>
+      dop === op &&
+      (dop1 === op1 || dop1 === op2 || dop2 === op1 || dop2 === op2),
+  );
+  const [dop1, , dop2, res] = foundDoor;
+  if (dop1 !== op1 && dop1 !== op2) {
+    return [res, dop1];
+  }
+  if (dop2 !== op1 && dop2 !== op2) {
+    return [res, dop2];
+  }
+  return [res];
 };
 
-// console.log(solve1());
+const solve2 = () => {
+  const wrongDoors = new Set();
+  let [carry] = findDoor(`x00`, "AND", `y00`);
+  let i = 2;
+  while (i < 45) {
+    const [currXor] = findDoor(`x${pad(i)}`, "XOR", `y${pad(i)}`);
+    const [lastAnd] = findDoor(`x${pad(i - 1)}`, "AND", `y${pad(i - 1)}`);
+    const [lastXor] = findDoor(`x${pad(i - 1)}`, "XOR", `y${pad(i - 1)}`);
+    const [op3, wrong1] = findDoor(carry, "AND", lastXor);
+    const [op2, wrong2] = findDoor(lastAnd, "OR", op3);
+    const [res, wrong3] = findDoor(currXor, "XOR", op2);
+    const zDoor = `z${pad(i)}`;
+
+    [wrong1, wrong2, wrong3].forEach((wrongDoor) => {
+      if (wrongDoor) {
+        wrongDoors.add(wrongDoor);
+      }
+    });
+    if (res !== zDoor) {
+      wrongDoors.add(zDoor);
+    }
+
+    carry = op2;
+    i++;
+  }
+  return [...wrongDoors].sort().join(",");
+};
+
+console.log(solve1());
 console.log(solve2());
